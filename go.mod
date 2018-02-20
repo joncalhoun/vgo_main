8cd81dda5b33e4dca4419fd6495607bc8c3374e8
Jon Calhoun <joncalhoun@gmail.com>
2018-02-20 18:38:24 -0500

    versioning demo - older != what many assumed.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module "github.com/joncalhoun/vgo_main"
+
+require "github.com/joncalhoun/vgo_demo" v1.0.1
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import demo "github.com/joncalhoun/vgo_demo"
+
+func main() {
+	demo.Log("hi!")
+}
